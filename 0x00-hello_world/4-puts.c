aef96c8fbc73c9bc541dc72ddf075515d41dd5aa
root <givenlonso233@gmail.com>
2023-02-25 01:37:44 -0800

    Puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - writes "Programming is like building a multilingual puzzle,
+ * followed by a new line.
+ * return 0.
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"/n);
+
+	return(0);
+}
