fd793cba4ad0b5e9ef0e1b067c6b6c679b238fe7
root <givenlonso233@gmail.com>
2023-02-25 01:45:13 -0800

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
  * main - writes "Programming is like building a multilingual puzzle,
  * followed by a new line.
- * return 0.
+ * Return: Always 0.
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"/n);
+	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 }
