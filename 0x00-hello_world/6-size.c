d64b8862b711b51ab4d8c5e3b654481b2f1d8936
root <givenlonso233@gmail.com>
2023-02-25 02:42:14 -0800

    size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the size of various types on the computer it is compiled and run on.
+ * Return: Always 0.
+ */
+
+{
+	char charType;
+	int integerType;
+	long int longIntType;
+	long long int long2IntType;
+	float floatType;
+
+	printf("Size of a char: %d byte(s)\n",sizeof(charType));
+	printf("Size of a int: %d byte(s)\n",sizeof(integerType));
+       	printf("Size of a long int: %d byte(s)\n",sizeof(longIntType));
+	printf("Size of a long long int: %d byte(s)\n",sizeof(long2IntType));
+	printf("Size of a float: %d byte(s)\n",sizeof(floatType);	
+	return (0);
+}
