65dfd8b0120f855d5693aa7dbb0014891b23a297
root <givenlonso233@gmail.com>
2023-02-25 05:21:47 -0800

    clever unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints and that piece of art is useful" - Dora Korpar, 2015-10=19
+ * Return: Always 1.
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
+
