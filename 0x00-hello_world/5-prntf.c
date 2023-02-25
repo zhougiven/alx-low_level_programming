414d30affa5d470e754410ee5bdb1fe18d67d6b5
root <givenlonso233@gmail.com>
2023-02-25 05:06:57 -0800

    Intel syntax

---
+++ 0x00-hello_world/5-prntf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * Main - prints "with proper grammar, but the outcome is a piece of art,"
+ * followed by a newline.
+ *Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,")
+	
+		return (0)
+
+}
