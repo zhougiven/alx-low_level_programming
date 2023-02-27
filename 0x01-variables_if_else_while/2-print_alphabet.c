765ddfcf11d3556ed883844793f02f6dd04c7843
zhougiven <givenlonso233@gmail.com>
2023-02-27 05:59:12 -0800

    Print aphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -prints all letter of the alphabet in lower cases.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	putchar (ch);
+	return (0);
+}
