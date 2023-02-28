ebcc96a6a81d70fd72ed9bb1c12054d21abf1514
zhougiven <givenlonso233@gmail.com>
2023-02-27 23:32:28 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers
+ * of base 10.
+ * variable char cannot be used.
+ * only can use putchar.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57, n++)
+	putchar (n);
+	putchar ('\n');
+	return (0);
+}
