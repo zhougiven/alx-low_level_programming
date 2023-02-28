e926a9f215cf4b3d5bbb10e38489b013dca9ce58
zhougiven <givenlonso233@gmail.com>
2023-02-28 05:48:59 -0800

    print combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible differents combinations
+ * of two digits separated by a comma
+ * and space.
+ * the two digits must be different.
+ * numbers will be in ascending order.
+ * putchar used max 5 times.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 56; i++)
+	{
+		for (j = 49; <= 57; j++)
+		{
+			if (j > i)
+			{
+				putchar (i);
+				putchar (j);
+				if (i != 56 || m != 57)
+				{
+					putchar (44);
+					putchar (32);
+				}
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
