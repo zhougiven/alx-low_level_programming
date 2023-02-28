e6f6730e52ca390c3631eb0fa8e8808f5338d7ef
zhougiven <givenlonso233@gmail.com>
2023-02-28 00:50:36 -0800

    Print combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all possible combinations
+ * of single digit numbers.
+ * number should be in ascending order.
+ * only  putchar can be used.
+ * putchar to be used max of 4 times in the code.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar (n);
+		if (n < 57)
+		{
+			putchar (44);
+			putchar (32);
+		}
+		putchar ('\n');
+		return (0);
+	}
+}
