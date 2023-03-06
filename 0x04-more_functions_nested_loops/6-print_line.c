a6e096a6a4295202931ef2435840e479a488630e
zhougiven <givenlonso233@gmail.com>
2023-03-05 23:46:10 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,7 +3,7 @@
 /**
  * print_line - draws a straight line in th terminal.
  * _putchar to print.
- * n is the number of times character _ should be printed.
+ * @n: is the number of times character _ should be printed.
  * the line should end with a newline.
  * if n is 0 or less, the functions prints /n.
  * Return: Success (0).
