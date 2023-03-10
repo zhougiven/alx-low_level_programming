7d23b46f8d0c5f0908b0c089947e7d98c909d7f6
zhougiven <givenlonso233@gmail.com>
2023-03-10 00:49:01 -0800

    2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - Entry point.
+ * @s: input.
+ * @c: input.
+ *
+ * Return: pointer to the first occurence of c in s, or NULL
+ * if not found.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] >= '\0')
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}
