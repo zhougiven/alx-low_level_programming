bcaa507694699251e8bb2cb50ff0d7510dcc0d07
zhougiven <givenlonso233@gmail.com>
2023-03-14 01:33:09 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,15 +2,19 @@
 
 /**
  * _puts_recursion - prints a string followed by newline.
- *
+ * @s: pointer variable.
  * Return: success.
  */
 
 void _puts_recursion(char *s)
 {
-	char i[] = {""};
-	*s = &i;
-
-	_putchar (*s);
-	_putchar ('\n');
+	if (*s)
+	{
+		_putchar (*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
