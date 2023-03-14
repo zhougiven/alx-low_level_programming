7e8fa82a2a9130131a6f24d5586db87717093415
zhougiven <givenlonso233@gmail.com>
2023-03-13 23:38:59 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,11 +4,13 @@
  * _pow_recursion - returns the value of x raised to the power of y.
  * Return: -1 if y < 0.
  * @y: y = 1 return x || y = 0 return 1.
+ * @x: input variable to be raised to the exponential value.
  */
 
 int _pow_recursion(int x, int y)
 {
 	int res, i;
+
 	res = x * x;
 
 	if (y < 0)
@@ -21,7 +23,7 @@ int _pow_recursion(int x, int y)
 	}
 	else if (y == 0)
 	{
-		return (0);
+		return (1);
 	}
 	else if (y == 2)
 	{
@@ -30,6 +32,7 @@ int _pow_recursion(int x, int y)
 	else
 	{
 		i = 3;
+
 		while (i <= y)
 		{
 			res = (res * x);
