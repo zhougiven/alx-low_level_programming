f8b3d9ccd06d978c66b9b09d93093c5763107aae
zhougiven <givenlonso233@gmail.com>
2023-03-15 05:59:49 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the program, followed by a newline.
+ * when renamed, the program prints the new new name without having
+ * to compile it again.
+ *
+ * Return: Success (0).
+ */
+
+int main(int argc, char *argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
