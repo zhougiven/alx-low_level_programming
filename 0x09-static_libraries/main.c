f2b47e26aa10019714ccfb74733a16e07c28f9a8
zhougiven <givenlonso233@gmail.com>
2023-03-15 02:01:18 -0700

    libmy.a, main.h

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,14 @@
 #include "main.h"
 
+/**
+ * main - prints out a strings using the _puts function from libmy.a library.
+ *
+ * Return: Success (0).
+ */
+
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+	_puts("\"At the end of the day, my goal was to be the best
+	hacker\"\n\t- Kevin Mitnick");
+	return (0);
 }
