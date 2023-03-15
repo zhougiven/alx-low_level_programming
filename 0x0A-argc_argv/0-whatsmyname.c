3f21c6a2b582d65b687e8ce8312871df67fd956c
zhougiven <givenlonso233@gmail.com>
2023-03-15 07:01:08 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
  * main - prints the name of the program, followed by a newline.
@@ -11,6 +11,7 @@
 
 int main(int argc, char *argv[])
 {
+	(void)argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
