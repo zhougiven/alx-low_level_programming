235d3ae51e2cce7cf2e18becd29cc4735793473e
zhougiven <givenlonso233@gmail.com>
2023-03-21 06:56:58 -0700

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - contatenates all arguments of the program.
+ * returns NULL if ac == 0 or av == NULL.
+ * @ac: number of arguments.
+ * @av: Array of arguments or strings.
+ * Return: Pointer to a new string, or NULL if it fails.
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *args;
+	int i;
+	size_t str_len;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < ac)
+	{
+		str_len = strlen(av[i]);
+		args = (char *)malloc(str_len + 1);
+		if (args == NULL)
+		{
+			return (NULL);
+		}
+		printf("%s\n", av[i]);
+		i++;
+	}
+	return (args);
+}
