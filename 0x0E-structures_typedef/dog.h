0762724695c11adb1948929560120bc70a4bbf24
zhougiven <givenlonso233@gmail.com>
2023-03-25 07:02:01 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - creates a new struct type.
+ * @name: first member.
+ * @age: second member.
+ * @owner: third member.
+ *
+ * Description: New user defined type, that is struct type,
+ * struct dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
