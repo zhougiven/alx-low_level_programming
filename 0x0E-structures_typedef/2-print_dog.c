e17185ba0e22929a459d5b2b89facb13bf079d62
zhougiven <givenlonso233@gmail.com>
2023-03-25 12:56:16 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -22,14 +22,7 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: %s\n", d->name);
 	}
-	if (d->age == 0)
-	{
-		printf("Age: 0.000000\n");
-	}
-	else
-	{
-		printf("Age: %f\n", d->age);
-	}
+	printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 	{
 		printf("Onwer: (nil)\n");
